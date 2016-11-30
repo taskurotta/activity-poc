f0ad883233226d816be08abe72e86c380506dbdf
Sazonenko Ivan <i.sazonenko@egron.net>
2016-11-30 14:48:11 +0700

    test config fixed for Spring Boot 1.4

--- src/test/java/org/activiti/HireProcessTest.java
+++ src/test/java/org/activiti/HireProcessTest.java
@@ -11,20 +11,16 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.subethamail.wiser.Wiser;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = {MyApp.class})
-@WebAppConfiguration
-@IntegrationTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HireProcessTest {
 
     @Autowired
